0e30136bbcd3993d0f7010a188dec866be3a911c
Bago0 <jgamadzebagrati@gmail.com>
2023-02-09 23:53:22 +0400

    loops_task2(update1)

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -4,19 +4,23 @@ public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
         int result = 0;
         int multiplied;
-        int time = 0;
+        int time;
+        if (multiplyByAndToInclusive == 0) {
+            System.out.print("");
+        } else {
         if (multiplyByAndToInclusive > 0) {
             time = multiplyByAndToInclusive;
-            time++;
-        }else{
+        } else  {
             time = -1 * multiplyByAndToInclusive;
-            time++;
         }
-        while (time!=0) {
-            multiplied = result * multiplyByAndToInclusive;
-            System.out.println(multiplied);
-            result++;
-            time--;
+
+            time++;
+            while (time != 0) {
+                multiplied = result * multiplyByAndToInclusive;
+                System.out.println(multiplied);
+                result++;
+                time--;
+            }
         }
     }
 }
